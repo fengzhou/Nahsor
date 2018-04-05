2efebd74b06e8862e163d74613d18d84582e63d5
Bai <fenyukuang@gmail.com>
2018-04-05 16:17:07 +0800

    report

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ app = Flask(__name__,static_url_path='')
 
 @app.route('/')
 def index():
-	return app.send_static_file('index.html')
+    # return render_template('请访问http://127.0.0.1:8808/reprot/index.html')
+    return '<h1>请访问http://127.0.0.1:8808/reprot/index.html</h1>'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8808, debug=True)
