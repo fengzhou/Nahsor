248121d05c60c42e1734a2e06ad8c3e9924d3e93
Bai <fenyukuang@gmail.com>
2018-04-19 22:15:47 +0800

    ex

---
+++ examples/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+    dict1 = request.get_json()
+    response = jsonify(dict1)
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2333, debug=True)
