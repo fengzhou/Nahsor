1160779ed7e76a57caeaa4a1ff798ec62b8a276e
Bai <fenyukuang@gmail.com>
2018-04-29 06:06:37 +0800

    优化程序结构。

--- app/core/runner.py
+++ app/core/runner.py
@@ -1,8 +1,10 @@
-from app.core.testutils import run_http_test
-from app.core.testutils import extract_global_values
-from app.core.testutils import run_validata_test, exex_global_values
+from app.core.testutils import run_http_test, get_validata_test
+from app.core.testutils import extract_global_values, run_execs_test, run_evals_test
+from app.core.testutils import run_validata_test, exec_global_values
 from app.utils.log import Logger
 from app.utils.jsonfuc import json_to_dict
+
+Logger = Logger()
 extracts = {}
 
 
@@ -23,32 +25,35 @@ class Runner(object):
         global extracts
         try:
             if testtype == "testsuite":
-                execlist = exex_global_values(request)
+                execlist = exec_global_values(request)
+                # print(execlist)
+                # run_execs_test(execlist)
                 if len(execlist) != 0:
                     for execkey in execlist:
-                        print(execkey)
+                        print("execkey:",execkey)
                         exec(execkey)
                 r = run_http_test(testname, request)
-                extlist = extract_global_values(extract)
-                for value in extlist:
-                    # print(value)
-                    exec(value)
+                execlist = extract_global_values(extract)
+                # print(evalist)
+                # run_execs_test(execlist)
+                if len(execlist) != 0:
+                    for execkey in execlist:
+                        print("execkey:",execkey)
+                        exec(execkey)
             else:
                 r = run_http_test(testname, request)
-            for validate in validates:
-                key = list(validate.keys())[0]
-                # print(key)
-                chicker = run_validata_test(key)
-                chicklist = []
-                for i in validate[key]:
-                    chicklist.append(eval(i))
-                try:
-                    chicker(chicklist)
-                    Logger().info("测试用例[%s]检查点执行通过,检查点信息为 --> %s" % (testname, chicklist))
-                except Exception as e:
-                    raise
-                    Logger().war("测试用例[%s]测试不通过,错误信息为 --> %s" % (testname, e))
+            
+            validatelist = get_validata_test(validates)
+            # run_validata_test(testname, validatelist)
+            if len(validatelist) != 0:
+                for validatekey in validatelist:
+                    res = eval(validatekey)
+                    if res == True:
+                        Logger.info("测试用例[%s]检查点执行成功,检查点信息为 --> %s" % (testname, validatekey))
+                    else:
+                        Logger.war("测试用例[%s]检查点执行失败,检查点信息为 --> %s" % (testname, validatekey))
+
         except Exception as e:
-            Logger().war("测试用例[%s]测试不通过,错误信息为 --> %s" % (testname, e))
+            Logger.war("测试用例[%s]测试不通过,错误信息为 --> %s" % (testname, e))
 
 
--- app/core/testutils.py
+++ app/core/testutils.py
@@ -1,34 +1,36 @@
 import requests
 from app.utils.log import Logger
 
+Logger = Logger()
 
 def run_http_test(testname, request):
     '''
     对HTTP接口发送请求
     '''
+    Logger.info("开始执行测试用例[%s]" % testname)
+    Logger.info("接口请求地址为 --> %s" % request["url"])
+    Logger.info("接口请求方法为 --> %s" % request["method"])
+    Logger.info("接口请求header --> %s" % request["headers"])
+    Logger.info("接口请求数据为 --> %s" % request["json"])
     try:
-        Logger().info("开始执行测试用例[%s]" % testname)
-        Logger().info("接口请求地址为 --> %s" % request["url"])
-        Logger().info("接口请求方法为 --> %s" % request["method"])
-        Logger().info("接口请求header --> %s" % request["headers"])
-        Logger().info("接口请求数据为 --> %s" % request["json"])
-
-
         r = requests.request(**request)
     except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as timeout:
-        Logger().error("测试用例[%s]在执行过程中出现异常，错误信息为 --> %s" % (testname, timeout))
-    try:
-        assert r.status_code == 200
+        Logger.error("测试用例[%s]在执行过程中出现异常，错误信息为 --> %s" % (testname, timeout))
+    Logger.info("接口响应时间为 --> %ss" % r.elapsed.total_seconds())
+    Logger.info("接口响应状态为 --> %s" % r.status_code)
+    Logger.info("接口响应内容为 --> %s" % r.text)
+    if r.status_code == 200:
         return r
-    except:
-        Logger().error("测试用例[%s]在执行过程中出现异常，错误信息为 --> [code:%s],[error:%s]" % (testname, r.status_code, r.text))
+    else:
+        Logger.error("测试用例[%s]在执行过程中出现异常，错误信息为 --> [code:%s],[error:%s]" % (testname, r.status_code, r.text))
         raise
 
-def exex_global_values(request):
+def exec_global_values(request):
     '''
+    替换request里带了$的参数为全局变量中的值。
     {
         "url": "http://127.0.0.1:2333/test",
-        "json": {'token': '$extracts["token"]'},
+        "json": {'token': '$token'},
         "method": "POST",
         "headers": {
             "Content-Type": "application/json"
@@ -43,12 +45,10 @@ def exex_global_values(request):
                 # print(key)
                 if '$' == request[key][key1][:1]:
                     # print(key1)
-                    execkey = "request['%s']['%s'] = %s" % (key, key1, request[key][key1][1:])
+                    execkey = "request['%s']['%s'] = extracts['%s']" % (key, key1, request[key][key1][1:])
                     execlist.append(execkey)
                     # print(execkey)
     return execlist
-
-
 
 
 def extract_global_values(extract):
@@ -58,39 +58,77 @@ def extract_global_values(extract):
         {"token":"r.json()["data"]"}
     ]
     '''
-    extlist = []
+    execlist = []
     for extdict in extract:
         key = list(extdict.keys())[0]
         value = extdict[key]
         extfuc = "extracts['%s'] = %s" % (key, value)
-        extlist.append(extfuc)
-    return extlist
+        execlist.append(extfuc)
+    return execlist
 
 
-def run_validata_test(key):
+def run_execs_test(execlist,*arg):
     '''
-    用例的校验方法
-    arg : validate = {}
+    执行赋值类型的表达式
+    extracts['token'] = 'ssdsfsdf4s6f54s6f1a3s'
+    '''
+    if len(execlist) != 0:
+        for execkey in execlist:
+            print(execkey)
+            exec(execkey)
+
+
+def run_evals_test(evalist):
+    '''
+    执行判断类型的表达式
+    "12233" == 'sdsdd'
+    '''
+    if len(evalist) != 0:
+        for evalkey in evalist:
+            print(evalkey)
+            eval(evalkey)
+
+
+def run_validata_test(testname, validatelist):
+    '''
+    测试用例的检查点执行
+    '''
+    if len(validatelist) != 0:
+        for validatekey in validatelist:
+            res = eval(validatekey)
+            if res == True:
+                Logger.info("测试用例[%s]检查点执行成功,检查点信息为 --> %s" % (testname, validatekey))
+            else:
+                Logger.war("测试用例[%s]检查点执行失败,检查点信息为 --> %s" % (testname, validatekey))
+
+
+def get_validata_test(validates):
+    '''
+    提取的校验方法
+    arg : [{"Equal": ["r.status_code", "200"]}]
     '''
     asserts = {
-        "Equal": equal,
-        "NotEqual": notequal
-        # "True": "is True",
-        # "False": "is False",
-        # "Is": "is",
-        # "IsNot": "is not",
-        # "IsNone": "is None",
-        # "IsNotNone": "is not None",
-        # "In": "in",
-        # "NotIn": "not in",
-        # "IsInstance": "isinstance",
-        # "NotIsInstance": "not isinstance"
+        "Equal": "==",
+        "NotEqual": "!=",
+        "True": "is True",
+        "False": "is False",
+        "Is": "is",
+        "IsNot": "is not",
+        "IsNone": "is None",
+        "IsNotNone": "is not None",
+        "In": "in",
+        "NotIn": "not in",
+        "IsInstance": "isinstance",
+        "NotIsInstance": "not isinstance"
     }
-    return asserts[key]
+    validatelist = []
+    for validate in validates:
+        key = list(validate.keys())[0]
+        # print(key)
+        asserts[key]
+        validate[key]
+        validatekey = validate[key][0] + asserts[key] + validate[key][1]
+        validatelist.append(validatekey)
 
-def equal(validate):
-    assert validate[0] == validate[1]
+    return validatelist
 
-
-def notequal(validate):
-    assert validate[0] != validate[1]
