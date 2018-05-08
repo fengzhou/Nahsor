bc75c8d6a6eb50f77327c306933c79229b3beec3
snake <snake_goodluck@hotmail.com>
2018-05-08 17:20:49 +0800

    test jenkins

--- run.py
+++ run.py
@@ -12,4 +12,4 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-    app.run(port=8808,threaded=True)
+    app.run(host="0.0.0.0", port=8808, threaded=True)
